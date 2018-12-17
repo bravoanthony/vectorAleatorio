2986c036d4f1f252684b682672891700b5188356
Estudiante PUCESE <estudiante@pucese.edu.ec>
2018-12-17 09:00:45 -0500

    vectoresa

---
+++ AppVectoresAleatorios/AppVectoresAleatorios/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppVectoresAleatorios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = new int[MAX];
+            leerVectores(x);
+        }
+        public static void imprimirVectores(int[] x)
+        {
+            for (int i=0;i < MAX; i++) { }
+        }
+    }
+}
